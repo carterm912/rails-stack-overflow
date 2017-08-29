ea9dbe537bae3df0b9f86f4c4f2295cbd28f66af
Blake Meyer & Carter McGee & younghoonkwon <pair+blakeynwa.carterm912.younghoonkwon@devbootcamp.com>
2017-08-28 18:02:53 -0700

    added devise user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,5 @@ class User < ApplicationRecord
   has_many :questions_answered, through: :answers_provided, source: :question
 
   validates :username, presence: true, uniqueness: true
-  validates :first_name, presence:true
-  validates :last_name, presence:true
   validates :email, presence:true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   # get 'questions/:id/answers/new'
 
   resources :questions do
